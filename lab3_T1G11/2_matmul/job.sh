ca107c999194eb8dc1f2d85bf2fe064ed9a61e60
Moca-15 <ritacasablancas@gmail.com>
2025-06-08 02:05:49 +0200

    lab3 ex 2 fatality

--- lab3_T1G11/2_matmul/job.sh
+++ lab3_T1G11/2_matmul/job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH -p gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --output=out_matmul.out
 #SBATCH --error=out_matmul.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:15:00
 
 module load nvhpc/24.9
 
 make >> make.out || exit 1
 
-./matmul 5 1
+./matmul 5000 1
 
